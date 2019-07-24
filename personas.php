4d61cc2550f01e82524604d87054bbb5a76be7e4
Chrystal Copeland <canderson73@cnm.edu>
2019-07-24 14:04:05 -0600

    added basic structure for Personas

---
+++ personas.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Outdoor Trails</title>
+
+	</head>
+	<body>
+		<h1>
+			Personas
+		</h1>
+
+		<h2>
+			Jake Powers
+		</h2>
+
+
+	</body>
+</html>
