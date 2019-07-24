6c3d35ec96e1b09b2221034c22498c5e3016b267
Chrystal Copeland <canderson73@cnm.edu>
2019-07-24 15:09:21 -0600

    renamed picture files

--- personas.php
+++ personas.php
@@ -11,8 +11,75 @@
 		</h1>
 
 		<h2>
-			Jake Powers
+			Persona 1 - Jake Powers
 		</h2>
+		<p>
+			age 26
+		</p>
+		<p><img src="jake-powers.jpg" alt="Jake Powers" height="500" width="500"/></p>
+		<p>
+			As a power user- I should be able to plan and save routes to my profile. I should be able to
+			plan future routes based on the routes I have saved.
+		</p>
+		<p>
+
+			Occupation: Engineer and Sandia National Labs.
+			He recently moved here for work and is currently renting a house near the base of the
+			Sandia Mountains. He has a laptop at home but usually uses his phone for finding information
+			on the internet. He owns an iPhone 10s.
+		</p>
+		<p>
+			Goal: He wants to locate  places to hike and bike on the weekends,
+			and also would like to  find a bike path he can take to work.
+			He is single and drives a used 2012 Toyota 4 Runner.
+		</p>
+		<p>
+			Frustrations: He would like to find a safe route to get to
+			Sandia National Labs. Traffic can be a bit hazardous on the main
+			streets and even if he had to take a longer route, he would take it to avoid
+			riding in a major street.
+		</p>
+		<hr />
+
+		<h2>
+			Persona 2 - Abby West
+		</h2>
+
+		<p>
+			Age 40
+		</p>
+		<p><img src="abby-west.jpg" alt="Abby West" height="500" width="650"/></p>
+
+		<p>
+			As a casual user- I should be able to view possible routes to fun destinations for my
+			family to use while on vacation in Albuquerque.
+		</p>
+		<p>
+			Occupation- tourist, blogger and stay at home mom.
+		</p>
+
+		<p>
+			Goal: Family vacation is planned for the balloon fiesta in Albuquerque.
+			The vacation rental she booked has bikes available for the family to use and
+			according to Trip Advisor, the best way to get to the balloon fiesta park is on
+			bike. She would like to map a route out for her family of 4 to get to the balloon
+			fiesta and avoid the traffic she has heard so much about regarding the balloon fiesta.
+			The family will be staying near Old Town Albuquerque and would also like to explore the
+			bosque and other bike trails while they are in town for their visit. They are an
+			active family and enjoy doing things outdoors together. Abby does most of her vacation
+			research on her laptop, but will be using her phone to navigate destinations once they
+			arrive in Albuquerque.
+		</p>
+
+		<p>
+			Frustrations - planning a trip to New Mexico for the balloon fiesta has been a bit
+			different than most of her vacation planning experiences. There is a lot of research
+			required, and planning involved to keep her family engaged and active on this vacation.
+			She wants to use our app to help keep her family active and not just cooped up in the
+			vacation rental for her entire vacation.
+
+		</p>
+
 
 
 	</body>
