90346b8e44ba5d4c002956fda451f0dcae0e76c4
Will Harris <will.t.harris@protonmail.com>
2019-07-29 14:53:41 -0600

    add state variables to Comments class

--- php/Classes/Comments.php
+++ php/Classes/Comments.php
@@ -10,4 +10,37 @@ use Ramsey\Uuid\Uuid;
 /**
  * Class declaration for the Comments class
  *
+ * This class will contain all the state variables and methods for any instance of Comments
+ * @package //TODO PUT NAMESPACE HERE
+ * @author wharris21
  **/
+class Comments {
+	use ValidateUuid;
+	use ValidateDate;
+
+	/**
+	 * id for this comment; this is the primary key
+	 * @var Uuid $commentId
+	 **/
+	private $commentId;
+	/**
+	 * route id for this comment; this is a foreign key
+	 * @var Uuid $commentsRouteId
+	 **/
+	private $commentsRouteId;
+	/**
+	 * user id for this comment; this is a foreign key
+	 * @var Uuid $commentsUserId
+	 **/
+	private $commentsUserId;
+	/**
+	 * string content of this comment
+	 * @var string $commentContent
+	 **/
+	private $commentContent;
+	/**
+	 * DateTime object containing the validated date
+	 * @var \DateTime $commentDate
+	 **/
+	private $commentDate;
+}
