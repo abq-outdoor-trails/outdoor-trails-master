c5a72db346d1c8ea65c25d904b555ffd7737960b
John Dunn <jdunn33@cnm.edu>
2019-07-29 14:13:33 -0600

    added comma after activation token, reformatted code

--- DDL/ddl-user.sql
+++ DDL/ddl-user.sql
@@ -1,5 +1,3 @@
-
-
 DROP TABLE IF EXISTS USER;
 
 
@@ -7,17 +5,17 @@ DROP TABLE IF EXISTS USER;
 
 CREATE TABLE user (
 	-- this creates the attribute for the primary key
- 	-- not null menas the attribute is required
- 	-- this is a strong entity
-	userId BINARY(16) NOT NULL,
-	userName VARCHAR(32) NOT NULL,
-	userEmail VARCHAR(128) NOT NULL,
-   userHash CHAR(97) NOT NULL,
-   userActivationToken CHAR(32)
-   -- to make sure duplicate data cannot exist, create a unique index
-   UNIQUE(userName),
-   UNIQUE (userEmail),
-   PRIMARY KEY(userId)
+	-- not null menas the attribute is required
+	-- this is a strong entity
+	userId              BINARY(16)   NOT NULL,
+	userName            VARCHAR(32)  NOT NULL,
+	userEmail           VARCHAR(128) NOT NULL,
+	userHash            CHAR(97)     NOT NULL,
+	userActivationToken CHAR(32),
+	-- to make sure duplicate data cannot exist, create a unique index
+	UNIQUE (userName),
+	UNIQUE (userEmail),
+	PRIMARY KEY (userId)
 
 
 );
