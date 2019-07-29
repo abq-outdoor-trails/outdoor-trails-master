43dff46af9ea5e5153a55737d2d22c7c1c3e7eee
Will Harris <will.t.harris@protonmail.com>
2019-07-29 14:41:49 -0600

    reformat directories, put Classes directory in php directory

---
+++ php/Classes/Comments.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ;
+
+require_once("autoload.php");
+require_once(dirname(__DIR__) . "/vendor/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+
+/**
+ * Class declaration for the Comments class
+ *
+ **/
