eb772a0b25a72aea46ce7a1e64b00559ffc03d24
John Dunn <jdunn33@cnm.edu>
2019-07-30 15:18:26 -0600

    adding activation token getter and setter

--- php/Classes/class-User.php
+++ php/Classes/class-User.php
@@ -97,7 +97,170 @@ use Ramsey\Uuid\Uuid;
 	public function setUserId($newUserId): void {
 		try {
 			$uuid = self::validateUuid($newUserId);
-					} catch(\InvalidArgumentException |\RangeException |\Exception |\TypeError $exception)
+					} catch(\InvalidArgumentException |\RangeException |\Exception |\TypeError $exception) {
+								$exceptionType = get_class($exception);
+								throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+
+		//convert and store the user id
+		$this->userId = $uuid;
+	}
+		/**
+		 * accessor method for account user name
+		 *
+		 * @return string value of the user name
+		 *
+	 	**/
+
+	public function getUserName() : ?string {
+				return ($this->userName)
 	}
 
+		/**
+		 * mutator method for account user name
+		 *
+		 * @param string $newUserName
+		 * @throws \InvalidArgumentException if string is too long or is insecure
+		 * @throws \RangeException if the userName is too long
+		 * @throws \TypeError if the userName is not a string
+		 *
+		 **/
+
+	public function setUserName(string $newUserName) : void {
+				//verify the userName is secure//
+				$newUserName = trim($newUserName);
+				$newUserName = filter_var($newUserName, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+				if(empty($newUserName) === true) {
+								throw(new \InvalidArgumentException("User Name is insecure"));
+
+				}
+
+				//store the user name//
+				$this->userName = $newUserName;
+	}
+
+		/**
+		 * accessor method for user email
+		 *
+		 * @return string value of email
+		 *
+		 **/
+
+	public function getUserEmail(): string {
+				return $this->userEmail;
+	}
+
+		/**
+		 * mutator for the user email
+		 *
+		 * @param string $newUserEmail new value of email
+		 * @throws \InvalidArgumentException if $newUserEmail is not a valid email of insecure
+		 * @throws \RangeException if $newUserEmail is > 128 characters
+		 * @throws \TypeError if $newUserEmail is not a strin
+		 *
+		 **/
+	public function setUserEmail(string $newUserEmail): void {
+
+				//verify the email will fit in the database//
+				$newUserEmail = trim($newUserEmail);
+				$newUserEmail = filter_var($newUserEmail, FILTER_VALIDATE_EMAIL);
+				if(empty($newUserEmail) === true) {
+							throw(new \InvalidArgumentException("user email is empty or insecure"));
+
+				}
+
+				//verify the email will fit in the database//
+				if(strlen($newUserEmail) > 128) {
+					throw(new \RangeException("user email is too large"));
+				}
+
+				//store the email//
+				$this->userEmail = $newUserEmail;
+	}
+
+				/**
+				 * accessor method for user hash
+				 *
+				 * @return string value of hash
+				 *
+				 **/
+
+		public function getUserHash(): string {
+					return $this->userHash;
+		}
+
+				/**
+				 *
+				 * mutator method for user hash
+				 *
+				 * @param string $newUserHash
+				 * @throws \InvalidArgumentException if the hash is not secure
+				 * @throws \RangeException if the hash is not 97 characters
+				 * @throws \TypeError if profile hash is not a string
+				 *
+				 **/
+
+		public function setUserHash(string $newUserHash): void {
+			//enforce that the hash is properly formatted//
+			$newUserHash = trim($newUserHash);
+			if(empty($newUserHash) === true) {
+						throw(new \InvalidArgumentException("user password hash empty or insecure"));
+
+			}
+
+			//enforce the hash is really an Argon hash//
+			$userHashInfo = password_get_info("$newUserHash");
+			if(userHashInfo["algoName"] !== "argon2i") {
+						throw(new \InvalidArgumentException("user hash is not a valid hash"));
+
+			}
+
+			//enforce that the hash is exactly 97 characters//
+			if(strlen($newUserHash) !==97) {
+							throw(new \RangeException("User hash must be 97 characters"));
+
+			}
+
+			//store the hash//
+			$this->userHash = $newUserHash;
+		}
+
+		/**
+		 * accessor method for user activation token
+		 *
+		 * @return string value of the activation token
+		 *
+		 **/
+	public function getUserActivationToken() : ?string {
+				return ($this->userActivationToken);
+	}
+
+		/**
+		 * mutator method for user activation token
+		 *
+		 * @param string $newUserActivationToken
+		 * @throws \InvalidArgumentException if the token is not a string or insecure
+		 * @throws \RangeException if the tokn is not exactly 32 characters
+		 * @throws \TypeError if the activation token is not a string
+		 *
+		 **/
+
+	public function setUserActivationToken(?string $newUserActivationToken): void {
+				if($newUserActivationToken === null) {
+				$this->userActivationToken = null;
+				return;
+				}
+
+				$newUserActivationToken = strtolower(trim($newUserActivationToken));
+				if(ctype_xdigit($newUserActivationToken) === false) {
+							throw(new\RangeException("user activation is not valid"));
+				}
+
+				//make sure user activation token is only 32 characters//
+				if(strlen($newUserActivationToken) !== 32) {
+						throw(new\RangeException("user activation token has to be 32"));
+
+				}
+				$this->userActivationToken = $newUserActivationToken;
+	}
 }
