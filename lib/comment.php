71bf98b1d216e775935c07ed8085c9fb4f51c930
Will Harris <will.t.harris@protonmail.com>
2019-07-31 14:44:47 -0600

    add lib directory and comment.php to try to instantiate Comment object

--- lib/comment.php
+++ lib/comment.php
@@ -5,4 +5,6 @@ require_once "../php/Classes/Comment.php";
 
 $comment = new Comment("379dae82-5a2b-4c4b-8193-b8e7749a3495", '379dae82-5a2b-4c4b-8193-b8e7749a3495', "379dae82-5a2b-4c4b-8193-b8e7749a3495", "This is a comment!  I really hope this works!", "2019-07-31 12:30:00.25");
 
+require_once "../Classes/Comment.php";
+
 var_dump($comment);
