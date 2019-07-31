be9ea9344edfb5902c8deca953ecb4039a4b38a3
Will Harris <will.t.harris@protonmail.com>
2019-07-31 11:26:26 -0600

    rename Comments to Comment, rename filename, begin building array of comments

--- php/Classes/Comment.php
+++ php/Classes/Comment.php
@@ -3,7 +3,7 @@
 namespace AbqOutdoorTrails\AbqBike;
 
 require_once("autoload.php");
-require_once(dirname(__DIR__,1) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 1) . "/vendor/autoload.php");
 
 use Ramsey\Uuid\Uuid;
 
@@ -276,7 +276,6 @@ class Comment implements \JsonSerializable {
 	 * @throws \Exception when other exceptions occur
 	 **/
 	public static function getCommentsByRouteId(\PDO $pdo, Uuid $routeId) : \SplFixedArray{
-		// validate routeId, throw error if invalid value
 		try {
 			$routeId = self::validateUuid($routeId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
@@ -287,53 +286,17 @@ class Comment implements \JsonSerializable {
 		$query = "SELECT commentId, commentRouteId, commentUserId, commentContent, commentDate FROM comments WHERE commentRouteId = :commentRouteId";
 		$statement = $pdo->prepare($query);
 		// bind the route id to the placeholder in the query template
-		$parameters = ["commentRouteId" => $routeId->getBytes()];
+		$parameters = ["commentRouteId" => $commentRouteId->getBytes()];
 		$statement->execute($parameters);
 		// build an array of comments
 		$comments = new \SplFixedArray($statement->rowCount());
 		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 		while($row = $statement->fetch()) {
 			try {
-				$comment = new Comment($row["commentId"], $row["commentRouteId"], $row["commentUserId"], $row["commentContent"], $row["commentDate"]);
-				$comments[$comments->key()] = $comment;
-				$comments->next();
-			} catch(\Exception $exception) {
-				// if the row couldn't be converted, rethrow it
-				throw(new \PDOException($exception->getMessage(), 0, $exception));
+				$comment = new Comment
 			}
 		}
-		return($comments);
 	}
-
-	/**
-	 * get comments by comment date, for display by most recent on the individual route view
-	 *
-	 *
-	 **/
-//	public static function getCommentsByCommentDate(\PDO $pdo, $commentDate) : \SplFixedArray {
-//		// validate date, throw error if invalid value
-//		try {
-//				$commentDate = self::validateDateTime($commentDate);
-//			} catch(\InvalidArgumentException | \RangeException | \Exception $exception) {
-//				$exceptionType = get_class($exception);
-//				throw(new $exceptionType($exception->getMessage(), 0, $exception));
-//			}
-//
-//		// create query template
-//		$query = "SELECT commentId, commentRouteId, commentUserId, commentContent, commentDate FROM comments WHERE commentDate = :commentDate";
-//		$statement = $pdo->prepare($query);
-//		// bind comment date to the placeholder in query template
-//		$parameters = ["commentDate" => $commentDate];
-//		$statement->execute($parameters);
-//		// build array of comments
-//		$comments = new \SplFixedArray($statement->rowCount());
-//		$statement->setFetchMode(\PDO::FETCH_ASSOC);
-//		while($row = $statement->fetch()) {
-//			try {
-//
-//			}
-//		}
-//	}
 
 	/**
 	 * formats the state variables for JSON serialization
