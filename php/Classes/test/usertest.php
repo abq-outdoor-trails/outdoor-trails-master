49785b585d8fc59b772cb7118afedfc7ca39e153
John Dunn <jdunn33@cnm.edu>
2019-08-01 15:28:11 -0600

    adding user test

--- php/Classes/test/usertest.php
+++ php/Classes/test/usertest.php
@@ -9,3 +9,14 @@ require_once(dirname(__DIR__) . "/autoload.php";
 //grab the uuid generator
 require_once(dirname(__DIR__, 2) . "/lib/uuid/php");
 
+/**
+ * Full PHP Unit test for User class
+ *
+ * This is a complete PHPUnit test of the User class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both valid and invalid inputs.
+ *
+ * @see User
+ * @author JDunn <jdunn33@cnm.edu>
+ *
+ **/
+
