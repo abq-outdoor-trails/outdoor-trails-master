d48e4f776555b997ed2c1680f1417f4b3412e534
Will Harris <will.t.harris@protonmail.com>
2019-08-01 16:34:16 -0600

    change comment case in ddl

--- DDL/abq-bike-trails.sql
+++ DDL/abq-bike-trails.sql
@@ -35,8 +35,8 @@ CREATE TABLE user (
 -- CREATE TABLE statement for comments table
 CREATE TABLE comments (
 	commentId BINARY(16) NOT NULL,
-	commentsRouteId BINARY(16) NOT NULL,
-	commentsUserId BINARY(16) NOT NULL,
+	commentRouteId BINARY(16) NOT NULL,
+	commentUserId BINARY(16) NOT NULL,
 	commentContent VARCHAR(256) NOT NULL,
 	commentDate DATE NOT NULL,
 	-- foreign keys for comments entity
