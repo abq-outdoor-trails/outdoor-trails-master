db656f2a61336f89089f8bceaa206b8b7adb882e
Will Harris <will.t.harris@protonmail.com>
2019-08-01 15:59:11 -0600

    add CommentTest file and add namespace and require_once statements

---
+++ php/Classes/Test/CommentTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace AbqOutdoorTrails\AbqBike\Test;
+
+use //TODO FIGURE OUT USE STATEMENT
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+
+/**
+ * Full PHPUnit test for the Comment class
+ **/
