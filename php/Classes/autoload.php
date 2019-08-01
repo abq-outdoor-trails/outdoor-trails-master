1f05cfcc6c81e09014d05c79ea24e45cdc644f58
Will Harris <will.t.harris@protonmail.com>
2019-08-01 10:45:26 -0600

    fix merge conflict

--- php/Classes/autoload.php
+++ php/Classes/autoload.php
@@ -15,6 +15,7 @@ spl_autoload_register(function($class) {
 	 * prefix: the prefix for all the classes (i.e., the namespace)
 	 * baseDir: the base directory for all classes (default = current directory)
 	 **/
+
 	$prefix = "AbqOutdoorTrails\\AbqBike";
 	$baseDir = __DIR__;
 
