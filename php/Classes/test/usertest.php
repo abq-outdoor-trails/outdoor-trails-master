c81e802b25d7cd76aed833cf10c75fd79f78e5b3
John Dunn <jdunn33@cnm.edu>
2019-08-01 15:16:10 -0600

    deleted vendor and composer lock

---
+++ php/Classes/test/usertest.php
@@ -0,0 +1,11 @@
+<?php
+namespace AbqOutdoorTrails\AbqBike;
+
+use Edu\Cnm\DataDesign\{User};
+
+//grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php";
+
+//grab the uuid generator
+require_once(dirname(__DIR__, 2) . "/lib/uuid/php");
+
