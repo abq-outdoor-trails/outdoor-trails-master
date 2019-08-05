b64e6ed7640ef4c00c5d9e145788ebb04514eb59
Will Harris <will.t.harris@protonmail.com>
2019-08-05 14:08:13 -0600

    add CommentTest declaration and docblock

--- php/Classes/Test/CommentTest.php
+++ php/Classes/Test/CommentTest.php
@@ -1,7 +1,7 @@
 <?php
 namespace AbqOutdoorTrails\AbqBike\Test;
 
-use //TODO FIGURE OUT USE STATEMENT
+use AbqOutdoorTrails\AbqBike\{User, Route};
 
 // grab the class under scrutiny
 require_once(dirname(__DIR__) . "/autoload.php");
@@ -10,5 +10,16 @@ require_once(dirname(__DIR__) . "/autoload.php");
 require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 /**
- * Full PHPUnit test for the Comment class
+ * Full PHPUnit test for the Comment class.  Tests *ALL* MySQL/PDO enabled methods for both invalid and valid inputs
+ *
+ * @see Comment
+ * @author wharris21@cmm.edu
  **/
+class CommentTest extends DataDesignTest {
+	/**
+	 * User that created the Comment; this is for foreign key relations
+	 *
+	 * @var User user
+	 **/
+	protected $user = null;
+}
