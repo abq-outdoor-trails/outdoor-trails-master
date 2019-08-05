62683775820c6850759400156896dd3b96451b09
Will Harris <will.t.harris@protonmail.com>
2019-08-05 14:17:21 -0600

    add variables to top of CommentTest

--- php/Classes/Test/CommentTest.php
+++ php/Classes/Test/CommentTest.php
@@ -15,11 +15,39 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
  * @see Comment
  * @author wharris21@cmm.edu
  **/
-class CommentTest extends DataDesignTest {
+class CommentTest extends AbqBikeTest {
 	/**
 	 * User that created the Comment; this is for foreign key relations
 	 *
 	 * @var User user
 	 **/
 	protected $user = null;
+
+	/**
+	 * Route the Comment is on; this is for foreign key relations
+	 *
+	 * @var Route route
+	 **/
+	protected $route = null;
+
+	/**
+	 * valid user hash to create the User object to own the test
+	 *
+	 * @var Uuid VALID_USER_HASH
+	 **/
+	protected $VALID_USER_HASH;
+
+	/**
+	 * content of the Comment
+	 *
+	 * @var string VALID_COMMENTCONTENT
+	 **/
+	protected $VALID_COMMENTCONTENT = "PHPUnit test passing?!?";
+
+	/**
+	 * timestamp of the comment; this starts as null and is assigned later
+	 *
+	 * @var \DateTime VALID_COMMENTDATE
+	 **/
+	protected $VALID_COMMENTDATE = null;
 }
