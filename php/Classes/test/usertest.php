244c4c1c0c3eb786bd15f3f8705d4c11c2eeb259
John Dunn <jdunn33@cnm.edu>
2019-08-05 14:28:21 -0600

    adding test user by valid email

--- php/Classes/test/usertest.php
+++ php/Classes/test/usertest.php
@@ -204,7 +204,7 @@ class UserTest extends DataDesignTest {
 
 			//grab the data from mySQL
 			$results = User::getUserByUserName($this->getPDO(), $this->VALID_USER_NAME);
-			$this->asserEquals($numRows +1, $this->getConnection()->getRowCount("user"));
+			$this->asserEquals($numRows + 1, $this->getConnection()->getRowCount("user"));
 
 			//enforce no other objects are bleeding into profile
 			$this->assertContainsOnlyInstancesOf("AbqOutdoorTrails\AbqBike\User", $results);
@@ -236,7 +236,47 @@ class UserTest extends DataDesignTest {
 		 **/
 		public function testGetValidUserByUserEmail() : void {
 			//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("user");
 
+		$userId = generateUuidV4();
+		$user = new User($userId, $this->VALID_USER_ID, $this->VALID_USER_NAME, $this->VALID_EMAIL, $this->VALID_HASH, $this->VALID_ACTIVATION);
+		$userId->insert($this->getPDO());
+
+		//grab the data from mySQL and enforce the fields match our expectations
+		$pdoUser = User::getUserByUserEmail($this->getPDO(), $user->getUserEmail());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("user"));
+		$this->assertEquals($pdoUser->getUserId(), $userId);
+		$this->assertEquals($pdoUser->getUserActivationToken(), $this->VALID_ACTIVATION);
+		$this->assertEquals($pdoUser->getUserName(), $this->VALID_USER_NAME);
+		$this->assertEquals($pdoUser->getUserEmail(), $this->VALID_EMAIL);
+		$this->assertEquals($pdoUser->getUserHash(), $this->VALID_HASH);
+
+		}
+
+		/**
+		 * test grabbing a User by an email that does not exist
+		 *
+		 **/
+		public function testGetInvalidUserByEmail() : void {
+			//grab and email that does not exist
+			$user = User::getUserByUserEmail($this->getPDO(), "does@not.exist");
+			$this->assetNull($user);
+
+		}
+
+		/**
+		 * test grabbing a User by its activation
+		 *
+		 **/
+		public function testGetValidUserByActivationToken() : void {
+			//count the number of rows and save it for later
+			$numRows = $this->getConnection()->getRowCount("user");
+
+			$pdoUserId = generateUuidV4();
+			$user = new User($userId, $this->VALID_USER_NAME, $this->VALID_USER_NAME, $this->VALID_EMAIL, $this->VALID_HASH, $this->VALID_ACTIVATION);
+			$user-insert($this->getPDO());
+
+			//
 		}
 }
 
