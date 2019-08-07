c0fc784e1fd1fd1e01dd3ea7c5f68e52d7beffb7
John Dunn <jdunn33@cnm.edu>
2019-08-07 08:48:08 -0600

    adding docblock for favoriteroute

--- php/Classes/Test/test-Favorite-Route.php
+++ php/Classes/Test/test-Favorite-Route.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace namespace AbqOutdoorTrails\AbqBike;
 
 use UssHopper\AbqBike\{
@@ -6,12 +7,15 @@ use UssHopper\AbqBike\{
 };
 
 //grab the class under scrutiny
-require_once (dirname(__DIR__) . "/autoload.php");
+require_once(dirname(__DIR__) . "/autoload.php");
 
 //grab the uuid generator
 require_once(dirname(__DIR__, 2) . "lib/uuid.php");
 
 /**
+ *Full PHPUnit test for FavoriteRoute class
  *
- *
+ * This is a complete PHPUnit test of the FavoriteRoute class.
+ * It is complete because *ALL* mySQL/PDO enabled methods are tested* *for both valid and invalid inputs.
  **/
+
