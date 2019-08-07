5b8e725140d12ac9bbdbe2e9bbdb4311adb7122f
John Dunn <jdunn33@cnm.edu>
2019-08-07 10:13:22 -0600

    adding set up for user

--- php/Classes/Test/test-Favorite-Route.php
+++ php/Classes/Test/test-Favorite-Route.php
@@ -2,8 +2,9 @@
 
 namespace namespace AbqOutdoorTrails\AbqBike;
 
+use AbqOutdoorTrails\AbqBike\Test\AbqBikeTest;
 use UssHopper\AbqBike\{
-	FavoriteRouteId, FavoriteRouteByUserId
+	User, Route
 };
 
 //grab the class under scrutiny
@@ -17,5 +18,77 @@ require_once(dirname(__DIR__, 2) . "lib/uuid.php");
  *
  * This is a complete PHPUnit test of the FavoriteRoute class.
  * It is complete because *ALL* mySQL/PDO enabled methods are tested* *for both valid and invalid inputs.
+ *
+ * @see FavoriteRoute
  **/
+
+class FavoriteRouteTest extends AbqBikeTest {
+	/**
+	 * User that created the favorite route; this is for foreign key relations
+	 * @var User $user
+	 **/
+
+	protected $user;
+
+	/**
+	 * Route that was Favorited; this is for foreign key relations
+	 *
+	 * @var FavoriteRoute $favoriteRoute
+	 *
+	 **/
+	protected $favoriteRoute;
+
+	/**
+	 * valid hash to use
+	 * @var $VALID_HASH
+	 *
+	 **/
+	protected $VALID_HASH;
+
+	/**
+	 * valid activationToken to create the user object to own the test
+	 * @var string $VALID_ACTIVATION
+	 *
+	 **/
+	protected $VALID_ACTIVATION
+
+	/**
+	 * create dependent objects before running each test
+	 *
+	 **/
+	public final function setUp() : void {
+		//run the default seUp() method first
+		parent::setUp():
+
+		//create a salt and hash for the mocked user
+		$password = "abc123";
+		$this->VALID_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
+		$this->VALID_ACTIVATION = bin2hex(random_bytes(16));
+
+		//create and insert the mocked profile
+		$this->user = new User(generateUuidV4(), 'willieharris', '@williesworld.cnm.ed', $this->VALID_HASH, $this->VALID_ACTIVATION);
+		$this->user->insert($this->getPDO());
+
+		//create the and insert the mocked route
+		$this->Route = new Route()
+	}
+
+
+
+
+
+
+	/**
+	 * favorite route by route ID; this is a foreign key
+	 * @ var favoriteRouteRouteID
+	 */
+
+	protected $VALID_ROUTE;
+
+	/**
+	 * favorite route by User ID; this is a foreign key
+	 * favoriteRouteUserId
+	 */
+	protected $VALID_FAVORITE_ROUTE_BY_USERID;
+}
 
