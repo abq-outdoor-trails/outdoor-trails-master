683ff5abe4f01b88089db90fd53a3abbdc08f404
John Dunn <jdunn33@cnm.edu>
2019-08-07 08:23:23 -0600

    deleting favoriteroutes file

--- php/Classes/Test/test-Favorite-Route.php
+++ php/Classes/Test/test-Favorite-Route.php
@@ -1,9 +1,17 @@
 <?php
-namespace AbqOutdoorTrails\AbqBike\Test;
+namespace namespace AbqOutdoorTrails\AbqBike;
 
-use UssHopper\DataDesign\{
-			FavoriteRouteId, FavoriteRouteUserId
+use UssHopper\AbqBike\{
+	FavoriteRouteId, FavoriteRouteByUserId
 };
 
 //grab the class under scrutiny
+require_once (dirname(__DIR__) . "/autoload.php");
 
+//grab the uuid generator
+require_once(dirname(__DIR__, 2) . "lib/uuid.php");
+
+/**
+ *
+ *
+ **/
