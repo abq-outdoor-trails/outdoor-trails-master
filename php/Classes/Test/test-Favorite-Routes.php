42896dfc5e7d224950ece06db5df68761da41b12
John Dunn <jdunn33@cnm.edu>
2019-08-06 19:46:10 -0600

    adding test favorite route namespace

--- php/Classes/Test/test-Favorite-Routes.php
+++ php/Classes/Test/test-Favorite-Routes.php
@@ -1,2 +1,17 @@
-<?php
+		<?php
+		namespace namespace AbqOutdoorTrails\AbqBike;
 
+		use UssHopper\AbqBike\{
+			FavoriteRouteId, FavoriteRouteByUserId
+		};
+
+		//grab the class under scrutiny
+		require_once (dirname(__DIR__) . "/autoload.php");
+
+		//grab the uuid generator
+		require_once(dirname(__DIR__, 2) . "lib/uuid.php");
+
+		/**
+		 *
+		 *
+		 **/
