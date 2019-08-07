3cc19ea947ff09823b0ab44e51e1ab9087f7d5df
John Dunn <jdunn33@cnm.edu>
2019-08-07 11:45:03 -0600

    adding deleting a favorite route

--- php/Classes/Test/test-Favorite-Route.php
+++ php/Classes/Test/test-Favorite-Route.php
@@ -4,7 +4,7 @@ namespace namespace AbqOutdoorTrails\AbqBike;
 
 use AbqOutdoorTrails\AbqBike\Test\AbqBikeTest;
 use UssHopper\AbqBike\{
-	User, Route
+	User, Route, FavoriteRoute
 };
 
 //grab the class under scrutiny
@@ -33,10 +33,10 @@ class FavoriteRouteTest extends AbqBikeTest {
 	/**
 	 * Route that was Favorited; this is for foreign key relations
 	 *
-	 * @var FavoriteRoute $favoriteRoute
+	 * @var Route $route
 	 *
 	 **/
-	protected $favoriteRoute;
+	protected $route;
 
 	/**
 	 * valid hash to use
@@ -70,8 +70,59 @@ class FavoriteRouteTest extends AbqBikeTest {
 		$this->user->insert($this->getPDO());
 
 		//create the and insert the mocked route
-		$this->Route = new Route()
+		$this->route = new Route(generateUuidV4(), 'Madison Bike Blvd', '21', 'bike blvd', 'Speed limit 25mph', 'bike blvd shared user road with lowered speed limit');
+		$this->route->insert($this->getPDO());
+
 	}
+
+	/**
+	 * test inserting a valid FavoriteRoute and verify the actual mySQL data matches
+	 *
+	 **/
+	public function testInsertValidFavoriteRoute() : void {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("favoriteRoute");
+
+		//create a new FavoriteRouteByRouteId and insert into mySQL
+		$favoriteRoute = new FavoriteRoute($this->user->getUserId(), $this->route->getRouteId());
+		$favoriteRoute->insert($this->getPDO());
+
+		//grab the data from mySQL and enforce the fields match our expectations
+		$pdoFavoriteRoute = FavoriteRoute::getFavoriteRouteByRouteId($this->getPDO(), $this->user->getUserId(), $this->route->getRouteId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("favoriteRoute"));
+		$this->assertEquals($pdoFavoriteRoute->getUserId(), $this->user->getUserId());
+		$this->assertEquals($pdoFavoriteRoute->getRouteId(), $this->route->getRouteId());
+
+
+	}
+
+	/**
+	 * test creating a FavoriteRouteByUserId and then deleting it
+	 *
+	 **/
+	public function testDeleteValidFavoriteRouteId() : void {
+		//count number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("FavoriteRoute");
+
+		//create a new FavoriteRouteById and insert into mySQL
+		$route = new FavoriteRoute($this->user->getUserId(), $this->route->getRouteId(), $this->VALID_ROUTE);
+		$route->insert($this->getPDO());
+
+		//delete the route from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("FavoriteRoute"));
+		$favoriteRoute->delete($this->getPDO());
+
+		//grab the data from mySQL and enforce the Route does not exist
+		$podRoute = FavoriteRoute::getRouteByIdAndRouteUserId($this->getPDO(), $this->user->getUserId(), $this->route->getRouteId());
+		$this->assertNull($pdoRoute);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("route"));
+
+	}
+
+	/**
+	 * test inserting a route and regrabbing it from mySQL
+	 *
+	 **/
 
 
 
