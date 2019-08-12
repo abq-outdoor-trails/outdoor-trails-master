c9b8067daad2cd9589058e96c2fa6861cf10f049
Chrystal Copeland <canderson73@cnm.edu>
2019-08-12 09:08:00 -0600

    adding insert method for route

--- php/Classes/Route.php
+++ php/Classes/Route.php
@@ -267,6 +267,23 @@ class Route implements \JsonSerializable {
 	}
 
 	/**
+	 * inserts route into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 */
+	public function insert(\PDO $pdo) : void {
+
+		//create query template
+		$query = "INSERT INTO route(routeId, routeName, routeFile, routeType, routeSpeedLimit, routeDescription) VALUES (:routeId, :routeName, :routeFile, :routeType, :routeSpeedLimit, :routeDescription)";
+		$statement = $pdo->prepare($query);
+
+		//bind the member variables to the place holders in the template
+		$parameters = ["routeId" => $this->routeId->getBytes(), "routeName" => $this->routeName->getBytes(), "routeFile" =>$this->routeFile, "routeFile" => $this->routeFile, ""]
+	}
+
+	/**
 	 * gets the getrouteByRouteId
 	 *
 	 * @param \PDO $pdo PDO connection object
