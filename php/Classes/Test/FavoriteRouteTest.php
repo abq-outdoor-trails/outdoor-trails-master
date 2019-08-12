bbb7f74ad995b6f1dffde86be4049c8df0ad356a
Will Harris <will.t.harris@protonmail.com>
2019-08-12 10:12:43 -0600

    fix pathing in FavoriteRouteTest

--- php/Classes/Test/FavoriteRouteTest.php
+++ php/Classes/Test/FavoriteRouteTest.php
@@ -9,7 +9,7 @@ use AbqOutdoorTrails\AbqBike\ { User, Route, FavoriteRoute };
 require_once(dirname(__DIR__) . "/autoload.php");
 
 //grab the uuid generator
-require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
+require_once(dirname(__DIR__, 3) . "/lib/uuid.php");
 
 /**
  *Full PHPUnit test for FavoriteRoute class
