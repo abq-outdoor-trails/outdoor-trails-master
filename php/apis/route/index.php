a053d8771b65cc16c08aaed9f85a4c3d97f1c71b
Chrystal Copeland <canderson73@cnm.edu>
2019-08-16 11:47:19 -0600

    completed api for route

--- php/apis/route/index.php
+++ php/apis/route/index.php
@@ -6,9 +6,7 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once ("/etc/apache2/capstone-mysql.Secrets.php");
 
-use route {
-
-};
+use AbqOutdoorTrails\AbqBike\{Route};
 
 /**
  * api for the route class
@@ -32,28 +30,45 @@ try {
 	$pdo = $secrets->getPdoObject();
 
 	// determine which HTTP method was used
-	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $SERVER["REQUEST_METHOD"];
+	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
 	//sanitize input
 	$id = filter_input(INPUT_GET, id, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$routeId = filter_input(INPUT_GET, "routeId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$routeDescription = filter_input(INPUT_GET, "routeDescription", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$routeFile - filter_input(INPUT_GET, "routeFile", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$routeName - filter_input(INPUT_GET, "routeName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$routeSpeedLimit - filter_input(INPUT_GET, "routeSpeedLimit", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$routeType - filter_input(INPUT_GET, "routeSpeedLimit", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$routeFile = filter_input(INPUT_GET, "routeFile", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$routeName = filter_input(INPUT_GET, "routeName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$routeSpeedLimit = filter_input(INPUT_GET, "routeSpeedLimit", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$routeType = filter_input(INPUT_GET, "routeSpeedLimit", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 
 	//make sure the id is valid for methods that require it
 
-	if(($method === "DELETE || $method === "PUT) && (empty(id) === true)) {
+	if(($method === "DELETE" || $method === "PUT") && (empty(id) === true)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative, 402"));
 	}
 
 	// handle GET request - if id is present, that route is returned, otherwise all routes are returned
-	if($method ==- "GET") {
+	if($method === "GET") {
 		//set XSRF cookie
-		setXSRFCookie();
+		setXsrfCookie();
 
-
+		// get a specific route or all routes and update reply
+		if(empty(id) === false) {
+			$reply->data = Route::getRouteByRouteId($pdo, $id);
+			// get a specific route by route type and update reply
+		} else if(empty($routeType) === false) {
+			$reply->data = Route::getRouteByRouteType($pdo, $routeId);
+			// get s specific route by route file and update reply
+		} else if(empty($routeFile) === false) {
+			$reply->data = Route::getRouteByRouteFile($pdo, $routeFile)->toArray;
+		}
 	}
+
+	// update the $reply->status $reply->message
+} catch(\Exception | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
 }
+
+header("Content-type: spplication/json");
+echo json_encode($reply);
