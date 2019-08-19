afe071e79d6ef6d38c703838061f73a39110a07b
Will Harris <will.t.harris@protonmail.com>
2019-08-19 08:46:38 -0600

    refactor signup api requires slightly

--- php/apis/sign-up/index.php
+++ php/apis/sign-up/index.php
@@ -1,9 +1,9 @@
 <?php
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/Classes/autoload.php";
-require_once("/etc/apache2/capstone-mysql/Secrets.php");
 require_once dirname(__DIR__, 3) . "/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/lib/uuid.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
 use AbqOutdoorTrails\AbqBike\User;
 
