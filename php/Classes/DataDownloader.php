3d2eba89c38b6047336ecb9c8c58138d5b23ba86
Will Harris <will.t.harris@protonmail.com>
2019-08-20 16:35:50 -0600

    add data downloader class to Classes directory

---
+++ php/Classes/DataDownloader.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AbqOutdoorTrails\AbqBike;
+
+require_once("autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
+require_once(dirname(__DIR__,2 ) . "/php/lib/uuid.php");
+
+class DataDownloader {
+
+}
