e9acb8f273fe498a8d1fe65e4591bcb7eccc1635
John Dunn <jdunn33@cnm.edu>
2019-08-21 14:28:22 -0600

    adding info to dd

--- php/Classes/DataDownloader.php
+++ php/Classes/DataDownloader.php
@@ -10,15 +10,26 @@ require_once(dirname(__DIR__,2 ) . "/php/lib/uuid.php");
 class DataDownloader {
 	public static function pullRoutes() {
 		$newRoutes = null;
-		$url = "https://res.cloudinary.com/abqbike/raw/upload/v1566336860/BikePaths_jev8gc.json";
+		$urlBase = "https://res.cloudinary.com/abqbike/raw/upload/v1566336860/BikePaths_jev8gc.json";
 		$secrets = new \Secrets("/etc/apache2/capstone-mysql/abqbiketrails.ini");
 		$pdo = $secrets->getPdoObject();
-		//$password = TODO figure out how we are going to deal with a password here (do we need a password even?)
-		//$hash TODO see above
 		// TODO not including any other class creation, don't think we need to for a Route
 
 		for($i = 0; $i <= 0; $i++) {
 			//TODO figure out the loopy
+
+			$newRoute = self::readDataJson($urlBase . "?pageIndex=" . $i);
+			$objectId->routeId;
+
+			foreach($newRoute as $objectId) {
+				$routeId = generateUuidV4();
+			}
+				$routeDescription = $value->Direction;
+
+			//grab route info from json
+			$objectId = "";
+			foreach()
+
 		}
 	}
 
@@ -31,6 +42,7 @@ class DataDownloader {
 			}
 			// decode the Json file
 			$jsonConverted = json_decode($jsonData);
+			var_dump($jsonConverted);
 			// format
 			$jsonFeatures = $jsonConverted->Result;
 			$newRoutes = \SplFixedArray::fromArray($jsonFeatures);
@@ -40,3 +52,4 @@ class DataDownloader {
 		return($newRoutes);
 	}
 }
+DataDownloader::pullRoutes();
