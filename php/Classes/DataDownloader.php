2ae3dd2ac2b84c191f0ddf37df8d2164b50d30cd
Chrystal Copeland <canderson73@cnm.edu>
2019-08-22 13:25:08 -0600

    trying to build the data downloader

--- php/Classes/DataDownloader.php
+++ php/Classes/DataDownloader.php
@@ -15,7 +15,7 @@ namespace AbqOutdoorTrails\AbqBike;
 **/
 
 require_once("autoload.php");
-require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 1) . "/vendor/autoload.php");
 require_once("/etc/apache2/capstone-mysql/Secrets.php");
 require_once(dirname(__DIR__, 2) . "/php/lib/uuid.php");
 
