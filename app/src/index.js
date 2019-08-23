20962e14abce8f72d7190e8dd1b3ac400dbe04cf
Will Harris <will.t.harris@protonmail.com>
2019-08-23 09:24:35 -0600

    add 404 and Home components, link in index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter } from "react-router-dom";
+import {Route, Switch} from "react-router";
+import {Home} from "./pages/Home";
+import {FourOhFour} from "./pages/FourOhFour";
 
-const App = () => ( <h1 className="text-info">hello world again?</h1> );
+
+const App = () => (
+	<>
+	 	<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route component={FourOhFour} />
+			</Switch>
+		 </BrowserRouter>
+	</>
+);
 ReactDOM.render(<App/>, document.querySelector('#root'));
