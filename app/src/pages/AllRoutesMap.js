fc58265ef912abb8346f60978aa9d0cf22b4c1c7
John Dunn <jdunn33@cnm.edu>
2019-08-25 14:07:34 -0600

    adding info to routes pages

--- app/src/pages/AllRoutesMap.js
+++ app/src/pages/AllRoutesMap.js
@@ -1,6 +1,22 @@
 import React from "react";
+import {UseWindowWidth} from "../../shared/components/UseWindowWidth";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Accordion from "react-bootstrap/Accordion";
+
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 
 export const AllRoutesMap = () => {
+
+	/**
+	 * const width holds the value of the screen width on resize event.
+	 * see UseWindowWidth
+	 *
+	 **/
 	return (
 		<>
 			<h1>Map</h1>
--- app/src/pages/SingleRoutePage.js
+++ app/src/pages/SingleRoutePage.js
@@ -4,7 +4,7 @@ export const SingleRotePage = () => {
 	return (
 		<>
 			<h1>Route</h1>
-			
+
 
 		</>
 	)
