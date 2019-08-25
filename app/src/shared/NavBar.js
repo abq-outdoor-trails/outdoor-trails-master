59a245ae32664c77d1b1eb79c525603ea7e602fb
Will Harris <will.t.harris@protonmail.com>
2019-08-25 13:52:59 -0600

    add navbar-styles rule to css, include in NavBar component

--- app/src/shared/NavBar.js
+++ app/src/shared/NavBar.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import '../index.css';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
 export const NavBar = () => {
 	return (
 		<>
-			<Navbar bg="dark" variant="dark">
-				<Navbar.Brand href="#home">
+			<Navbar bg="dark" variant="dark" className="navbar-styles">
+				<Navbar.Brand href="#home" className="navbar-styles">
 					AbqBike
 				</Navbar.Brand>
-				<Nav.Link href="#sign-in" className="ml-auto">Sign In</Nav.Link>
-				<Nav.Link href="#sign-up">Sign Up</Nav.Link>
+				<Nav.Link href="#sign-in" className="ml-auto navbar-styles">Sign In</Nav.Link>
+				<Nav.Link href="#sign-up" className="navbar-styles">Sign Up</Nav.Link>
 			</Navbar>
 		</>
 	)
