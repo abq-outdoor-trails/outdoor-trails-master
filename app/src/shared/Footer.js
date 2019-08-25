b41d9994914b142d0a896d5c376d6be9b3cb5076
Will Harris <will.t.harris@protonmail.com>
2019-08-25 13:56:18 -0600

    add footer and brand styles, include in Footer component

--- app/src/shared/Footer.js
+++ app/src/shared/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "../index.css";
 import Navbar from "react-bootstrap/Navbar";
 
 export const Footer = () => {
 	return (
 		<>
-			<Navbar bg="dark" variant="dark">
-				<Navbar.Brand href="#home">AbqBike</Navbar.Brand>
+			<Navbar bg="dark" variant="dark" className="footer-styles">
+				<Navbar.Brand href="#home" className="brand-styles">AbqBike</Navbar.Brand>
 			</Navbar>
 		</>
 	)
