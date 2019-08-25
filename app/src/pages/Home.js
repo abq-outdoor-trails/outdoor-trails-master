f81886fb53dc2317d58c41f04f6d2e2e264d10c8
Will Harris <will.t.harris@protonmail.com>
2019-08-25 12:50:12 -0600

    start footer component, import into home page

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
+import {Footer} from "../shared/Footer";
 
 export const Home = () => {
 	return (
 		<>
 			<h1>Home</h1>
 			<p>Welcome to ABQ Bike Trails! </p>
-
+			<Footer />
 		</>
 	)
 };
---
+++ app/src/shared/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+
+export const Footer = () => {
+	return (
+		<>
+			<Navbar bg="dark" variant="dark">
+				<Navbar.Brand href="#home">AbqBike</Navbar.Brand>
+			</Navbar>
+		</>
+	)
+};
