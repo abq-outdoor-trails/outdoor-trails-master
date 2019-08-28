541235578672a1b96929a03541cfc88178862088
Chrystal Copeland <canderson73@cnm.edu>
2019-08-28 15:28:41 -0600

    adding ui routes on index.js

--- app/src/index.js
+++ app/src/index.js
@@ -9,11 +9,12 @@ import "./index.css";
 import {Header} from "./shared/Header";
 import {Footer} from "./shared/Footer";
 import {Home} from "./pages/Home";
-import {AboutUs}
+import {AboutUs} from "./pages/AboutUs";
+import {Signup} from "./pages/Signup";
 import {FourOhFour} from "./pages/FourOhFour";
 
 
-import {Signup} from "./pages/Signup";
+import { library } from '@fortawesome/fontawesome-svg-core';
 import {
 	faEnvelope,
 	faPencilAlt,
@@ -26,7 +27,7 @@ import {
 import {AllRoutesMap} from "./pages/AllRoutesMap";
 import {SingleRotePage} from "./pages/SingleRoutePage";
 
-import { library } from '@fortawesome/fontawesome-svg-core';
+
 
 
 library.add(faPencilAlt, faUserCircle, faSortDown, faEnvelope, faKey, faSignInAlt, faDog );
@@ -37,12 +38,13 @@ const App = () => (
 	 	<BrowserRouter>
 			<Header/>
 			<Switch>
+				<Route exact path="/AllRoutesMap" component={AllRoutesMap} />
+				<Route component={FourOhFour} />
 				<Route exact path="/" component={Home} />
 				<Route exact path="/signup" component={Signup} />
-				/*<Route exact path="/route/:routeId" component={Route} /> */
-				<Route exact path="/AllRoutesMap" component={AllRoutesMap} />
 				<Route exact path="/SingleRoutePage" component={SingleRotePage} />
-				<Route component={FourOhFour} />
+				<Route exact path="/UserPage" component={UserPage} />
+
 			</Switch>
 		 </BrowserRouter>
 	</>
