453a34dc74c54b4d472f48a81e7bb71a5231ee9c
John Dunn <jdunn33@cnm.edu>
2019-08-28 15:58:23 -0600

    adding image tag

--- app/src/pages/SingleRoutePage.js
+++ app/src/pages/SingleRoutePage.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Image from "react-bootstrap/Image";
 
 export const SingleRoutePage = () => (
 
 <>
 	Single Route
 
-	
+	<Image src="images/abqmap.png" fluid />
 
 </>
 );
