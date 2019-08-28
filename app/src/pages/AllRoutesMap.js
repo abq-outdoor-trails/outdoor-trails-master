2060b64e71ff50e8546dd3b15d3ae785198246d2
John Dunn <jdunn33@cnm.edu>
2019-08-28 11:29:06 -0600

    deleted all files

--- app/src/pages/AllRoutesMap.js
+++ app/src/pages/AllRoutesMap.js
@@ -1,39 +1,9 @@
 import React from 'react';
-import Header from 'header';
-
-
-
-		<>
-			This is the All Routes Map
-		</>
-)
-import React from "react";
-import {UseWindowWidth} from "../../shared/components/UseWindowWidth";
-
-
+import (header) from ""
 
 
 export const AllRoutesMap = () => {
 
-	/**
-	 * const width holds the value of the screen width on resize event.
-	 * see UseWindowWidth
-	 *
-	 **/
-	return (
-		<>
-			<h1>Map</h1>
-			<main className="d-flex align-items-center mh-80">
-				<Container fluid="true">
-					<Row>
-						<Card.Body>
-							<Form>
 
-								<Form.Group>
-									<InputGroup>
-
-
-
-		</>
-	)
 };
+
