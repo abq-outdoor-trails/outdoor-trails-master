14388e63435c0123ff963c143bd8e0c7d43fa183
Chrystal Copeland <canderson73@cnm.edu>
2019-08-28 10:19:56 -0600

    changed index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter } from "react-router-dom";
 import {Route, Switch} from "react-router";
 import {Home} from "./pages/Home";
 import {FourOhFour} from "./pages/FourOhFour";
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 import "./index.css";
 import {Header} from "./shared/Header";
