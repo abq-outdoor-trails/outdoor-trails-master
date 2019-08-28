edaf0bea10689b817d1777b8fea1fd0d4772e235
Chrystal Copeland <canderson73@cnm.edu>
2019-08-28 11:36:59 -0600

    updated index.js

--- app/src/pages/AllRoutesMap.js
+++ app/src/pages/AllRoutesMap.js
@@ -4,27 +4,3 @@ import {UseWindowWidth} from "../../shared/components/UseWindowWidth";
 
 
 
-export const AllRoutesMap = () => {
-
-	/**
-	 * const width holds the value of the screen width on resize event.
-	 * see UseWindowWidth
-	 *
-	 **/
-	return (
-		<>
-			<h1>Map</h1>
-			<main className="d-flex align-items-center mh-80">
-				<Container fluid="true">
-					<Row>
-						<Card.Body>
-							<Form>
-
-								<Form.Group>
-									<InputGroup>
-
-
-
-		</>
-	)
-};
