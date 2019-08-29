6e41a6020b8318ee4bc86200258b5fbb77b0c121
Will Harris <will.t.harris@protonmail.com>
2019-08-29 14:50:02 -0600

    remove extra import from footer

--- app/src/shared/Footer.js
+++ app/src/shared/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
+
 import "../index.css";
-import Navbar from "react-bootstrap/Navbar";
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
