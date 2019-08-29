5ad045906754f9e39e4c0fa6f7753a13075f66df
Will Harris <will.t.harris@protonmail.com>
2019-08-29 15:24:03 -0600

    add beginning of boilerplate for sign-up page

--- app/src/pages/Signup.js
+++ app/src/pages/Signup.js
@@ -1,8 +1,30 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
 export const Signup = () => (
-		<>
-		This is the sign up page.
-		</>
-	);
+	<main className="d-flex align-items-center">
+		<Container fluid="true">
+			<Row>
+				<Col sm={6} lg={{span: 4, offset: 1}}>
+					<Card>
+						<Card.Body>
+							<Form>
+								<Form.Group>
+									<InputGroup>
+
+									</InputGroup>
+								</Form.Group>
+							</Form>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	</main>
+);
 
