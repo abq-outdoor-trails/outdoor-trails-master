bcd79304f921a98048d14e6a2efba73244487881
Will Harris <will.t.harris@protonmail.com>
2019-08-29 16:12:19 -0600

    change return format for footer

--- app/src/shared/Footer.js
+++ app/src/shared/Footer.js
@@ -2,12 +2,10 @@ import React from "react";
 import "../index.css";
 import Navbar from "react-bootstrap/Navbar";
 
-export const Footer = () => {
-	return (
+export const Footer = () => (
 		<>
 			<Navbar bg="dark" variant="dark" className="footer-styles">
 				<Navbar.Brand href="#home" className="brand-styles">AbqBike</Navbar.Brand>
 			</Navbar>
 		</>
-	)
-};
+);
