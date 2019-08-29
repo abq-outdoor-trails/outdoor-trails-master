dafdbc3056b052479020615b2db4952191b8b8e6
Will Harris <will.t.harris@protonmail.com>
2019-08-29 14:17:58 -0600

    add header boilerplate

--- app/src/shared/Header.js
+++ app/src/shared/Header.js
@@ -1,15 +1,37 @@
 import React from 'react';
+
 import '../index.css';
+
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 export const Header = () => {
 	return (
 		<>
 			<Navbar bg="dark" variant="dark" className="navbar-styles">
-				<Navbar.Brand href="#home" className="brand-styles">
-					AbqBike
-				</Navbar.Brand>
+				<Navbar.Brand href="#home" className="brand-styles">AbqBike</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#link">Link</Nav.Link>
+						<NavDropdown title="Dropdown" id="basic-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+					<Form inline>
+						<FormControl type="text" placeholder="Search" className="mr-sm-2" />
+						<Button variant="outline-success">Search</Button>
+					</Form>
+				</Navbar.Collapse>
 				<Nav.Link href="#sign-in" className="ml-auto navbar-styles">Sign In</Nav.Link>
 				<Nav.Link href="#sign-up" className="navbar-styles">Sign Up</Nav.Link>
 			</Navbar>
