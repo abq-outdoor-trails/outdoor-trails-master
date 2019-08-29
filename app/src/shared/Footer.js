555e814bb81d58ab00cb8eddb842641896b952c7
Will Harris <will.t.harris@protonmail.com>
2019-08-29 16:13:12 -0600

    remove footer content from navbar branch

--- app/src/shared/Footer.js
+++ app/src/shared/Footer.js
@@ -2,22 +2,6 @@ import React from "react";
 
 import "../index.css";
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+export const Footer = () => (
 
-export const Footer = () => {
-	return (
-		<>
-			<footer className="page-footer text-muted py-2 py-md-4">
-				<Container fluid="true">
-					<Row>
-						<Col className="text-center">
-							Github Logo Here
-						</Col>
-					</Row>
-				</Container>
-			</footer>
-		</>
-	)
-};
+);
