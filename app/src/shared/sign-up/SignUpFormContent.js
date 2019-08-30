d6e363bad412118798b0f48bf252b9efc75c734f
John Dunn <jdunn33@cnm.edu>
2019-08-30 13:41:56 -0600

    starting sign up form

---
+++ app/src/shared/sign-up/SignUpFormContent.js
@@ -0,0 +1,18 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {FormDebugger} from "../../FormDebugger";
+import React from "react";
+
+export const SignUpFormContent = (props) => {
+	const {
+				submitStatus,
+				values,
+				errors,
+				touched,
+				dirty,
+				isSubmitting,
+				handleChange,
+				handleBlur,
+				handleSubmit,
+				handleReset
+	}
+}
