5011a9ec2e9f7df93401e38cfffa24bdceea5ced
John Dunn <jdunn33@cnm.edu>
2019-08-30 15:23:27 -0600

    adding form

--- app/src/shared/sign-up/SignUpFormContent.js
+++ app/src/shared/sign-up/SignUpFormContent.js
@@ -28,6 +28,30 @@ export const SignUpFormContent = (props) => {
 							</div>
 						</div>
 						<input
+								className="form-control"
+								id="userEmail"
+								type="email"
+								value={values.userEmail}
+								placeholder="Enter email"
+								onChange={handleChange}
+								onBlur={handleBlur}
+
+
+								/>
+
+					</div>
+					{
+						errors.userEmail && touched.userEmail && (
+							<div className="alert alert-danger">
+								{errors.userEmail}
+							</div>
+						)
+					}
+
+				</div>
+				
+
+
 
 
 			</>
