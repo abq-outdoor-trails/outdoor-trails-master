c2844471977658f1c511f29833284eac6bca8ec8
John Dunn <jdunn33@cnm.edu>
2019-08-31 11:27:06 -0600

    adding forms and submit button

--- app/src/shared/sign-up/SignUpFormContent.js
+++ app/src/shared/sign-up/SignUpFormContent.js
@@ -49,10 +49,82 @@ export const SignUpFormContent = (props) => {
 					}
 
 				</div>
-				
+				{/*controlId must match what is defined by the initialValues object*/}
+				<div className="form-group">
+					<label htmlFor="userHash">Password</label>
+					<div className="input-group">
+						<div className="input-group-prepend">
+							<div className="input-group-text">
+								<FontAwesomeIcon icon="key">
+
+								</div>
+						</div>
+						<input
+									id="userHash"
+									className="form-control"
+									type="password"
+									placeholder="Password"
+									value={values.userHash}
+									onChange={handleChange}
+									onBlur={handleBlur}
+						/>
+					</div>
+					{errors.userHash && touched.userHash && (
+						<div className="alert alert-danger">{errors.userHashConfirm}</div>
+
+					)}
+				</div>
+
+				<div className="form-group">
+					<label htmlFor="userName">User Name</label>
+					<div className="input-group">
+						<div className="input-group-prepend">
+							<div className="input-group-text">
+								<FontAwesomeIcon icon="dove"/>
+
+							</div>
+					</div>
+					<input
+								className="form-control"
+								id="userName"
+								type="text"
+								value={values.userName}
+								placeholder="UserName"
+								onChange={handleChange}
+								onBlur={handleBlur}
+
+					/>
+				</div>
+					{
+							errors.userName && touched.userName && (
+								<div className="alert alert-danger">
+									{errors.userName}
+							)
+					}
+								</div>
+
+					}
+				</div>
+				<div className="form-group">
+					<button className="btn btn btn-danger mb-2" type="submit"Submit</button>
+				<button
+							className="btn btn-danger mb-2"
+							onClick={handleReset}
+							disabled={!dirty || isSubmitting}
+					>Reset
+				</button>
+			</div>
+
+					<FormDebugger {...props} />
+				</form>
+				{console.log(
+					submitStatus
+				)}
+				{
+					submitStatus && (<div className={submitStatus.type}>{submitStatus.message}</div>)
+				}
+				</>
 
 
-
-
-			</>
-	)
+		)
+};
