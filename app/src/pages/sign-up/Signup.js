ac3fb00110e1f007cb8b56038a76f9d2830189de
Will Harris <will.t.harris@protonmail.com>
2019-08-31 16:27:02 -0600

    remove erroneous imports from signup page

--- app/src/pages/sign-up/Signup.js
+++ app/src/pages/sign-up/Signup.js
@@ -6,11 +6,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
 import Spinner from "react-bootstrap/Spinner";
-import Button from "react-bootstrap/Button";
 
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
