f984e61fb0a1031b06116982391b5df3f5abcd70
John Dunn <jdunn33@cnm.edu>
2019-08-31 13:42:26 -0600

    adding form and debugger

--- app/src/shared/components/header/SignInFormConent.js
+++ app/src/shared/components/header/SignInFormConent.js
@@ -47,9 +47,40 @@ export const SignInFormContent = (props) => {
 								}
 							</div>
 							{/*controlId must match what is defined by the intialValues object*/}
-							
-								}
-								}
+							<div className="form-group">
+								<label htmlFor="userHash">Password</label>
+								<div className="input-group">
+									<div className="input-group-prepend">
+										<div className="input-group-text">
+											<FontAwesomeIcon icon="key"/>
+										</div>
+									</div>
+									<input
+												id="userHash"
+												className="form-control"
+												type="password"
+												placeholder="Password"
+												value={values.userHash}
+												onChange={handleChange}
+												onBlur={handleBlur}
+											/>
+								</div>
+
+								<div className="form-group">
+									<button className="btn btn-primary mb-2" type="submit">Submit</button>
+									<button
+												className="btn btn-danger mb-2"
+												onClick={handleReset}
+												disabled={!dirty || isSubmitting}
+											>Reset
+									</button>
+								</div>
+								<FormDebugger {...props}/>
+							</form>
+							{status && (<div className={status.type}->{status.message}</div>)}
+					</>
+
+
 
 		)
 }
