4ba889d45403a5fdb85078f1a054522aa933b0a2
Will Harris <will.t.harris@protonmail.com>
2019-08-31 13:40:01 -0600

    finish imports, define state vars, init state, init yup validator, define sign up form submit handler

--- app/src/pages/sign-up/SignUpForm.js
+++ app/src/pages/sign-up/SignUpForm.js
@@ -1,2 +1,46 @@
 import React, {useState} from 'react';
-import {httpConfig} from "../../../"
+import {httpConfig} from "../../shared/utils/http-config";
+import * as Yup from "yup";
+import {Formik} from "formik";
+
+import {SignUpFormContent} from "./SignUpFormContent";
+
+// define 'state variables' to be used in sign up form
+export const SignUpForm = () => {
+	const signUp = {
+		userName: "",
+		userEmail: "",
+		userPassword: "",
+		userPasswordConfirm: "",
+	};
+
+	// set status and setStatus to null initial state
+	const [status, setStatus] = useState(null);
+	// init yup validator
+	const validator = Yup.object().shape({
+		userName: Yup.string()
+			.name("user name cannot be empty")
+			.required("user name is required"),
+		userEmail: Yup.string()
+			.email("email must be a valid email")
+			.required("email is required"),
+		userPassword: Yup.string()
+			.required("password is required")
+			.min(8, "password must be at least 8 characters"),
+		userPasswordConfirm: Yup.string()
+			.required("please confirm password")
+			.min(8, "password must be at least 8 characters")
+	});
+
+	// define sign up form submit handler
+	const submitSignUp = (values, {resetForm}) => {
+		httpConfig.post("/apis/sign-up/", values)
+			.then(reply => {
+				let { message, type } = reply;
+				setStatus({ message, type });
+				if(reply.status === 200) {
+					resetForm();
+				}
+			});
+	}
+};
