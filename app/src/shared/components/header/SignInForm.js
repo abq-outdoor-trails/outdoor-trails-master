13a1749df6303f29309a1d287a9b35b9d1268791
Chrystal Copeland <canderson73@cnm.edu>
2019-09-02 10:49:51 -0600

    working on debugging form

--- app/src/shared/components/header/SignInForm.js
+++ app/src/shared/components/header/SignInForm.js
@@ -9,48 +9,45 @@ export const SignInForm = () => {
 	const validator = Yup.object().shape({
 		userEmail: Yup.string()
 			.email("email must be a valid email")
-			.required("email is required"),
+			.required('email is required'),
 		userHash: Yup.string()
 			.required("Password is required")
-				.min(8, "Password must be at least 8 characters")
+			.min(8, "Password must be at least 8 characters")
 	});
 
-		//the initial values object defines what the request payload is.
+	//the initial values object defines what the request payload is.
 	const signIn = {
-			userEmail: "",
-			userHash: ""
+		userEmail: "",
+		userHash: ""
 	};
 
 	const submitSignIn = (values, {resetForm, setStatus}) => {
 		httpConfig.post("/apis/sign-in", values)
 			.then(reply => {
-					let{message, type} = reply;
-					setStatus({message, type});
-					if(reply.status === 200 && reply.headers["x-jwt-token"]) {
-						window.localStorage.removeItem("jwt-token");
-						window.localStorage.setItem("jwt-toekn", reply.headers["x-jwt-token"]);
-						resetForm();
-					}
+				let {message, type} = reply;
+				setStatus({message, type});
+				if(reply.status === 200 && reply.headers["x-jwt-token"]) {
+					window.localStorage.removeItem("jwt-token");
+					window.localStorage.setItem("jwt-toekn", reply.headers["x-jwt-token"]);
+					resetForm();
+					setTimeout(() => {
+						window.location = "/posts";
+					}, 1500);
+				}
+				setStatus({message, type});
 
-			})
+			});
 	};
 
 	return (
-		<>
-					<Formik
-						initialValues={signIn}
-						onSubmit={submitSignIn}
-						validationSchema={validator}
-					>
-						{SignInFormContent}
-					</Formik>
-		</>
+		<Formik
+			initialValues={signIn}
+			onSubmit={submitSignIn}
+			validationSchema={validator}
+		>
+			{SignInFormContent}
+		</Formik>
 	)
-
-
-
-
-
 
 
 };
