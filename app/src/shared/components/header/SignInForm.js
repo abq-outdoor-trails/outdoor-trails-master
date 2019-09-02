5119102d8f4ed2cc7928db5dc262621007b6231b
Chrystal Copeland <canderson73@cnm.edu>
2019-09-02 10:54:28 -0600

    re-organized sign in code to match octo meow example

--- app/src/shared/components/header/SignInForm.js
+++ app/src/shared/components/header/SignInForm.js
@@ -6,6 +6,11 @@ import {AxiosInstance as httpConfig} from "axios";
 
 
 export const SignInForm = () => {
+	//the initial values object defines what the request payload is.
+	const signIn = {
+		userEmail: "",
+		userHash: ""
+	};
 	const validator = Yup.object().shape({
 		userEmail: Yup.string()
 			.email("email must be a valid email")
@@ -15,17 +20,12 @@ export const SignInForm = () => {
 			.min(8, "Password must be at least 8 characters")
 	});
 
-	//the initial values object defines what the request payload is.
-	const signIn = {
-		userEmail: "",
-		userHash: ""
-	};
+
 
 	const submitSignIn = (values, {resetForm, setStatus}) => {
 		httpConfig.post("/apis/sign-in", values)
 			.then(reply => {
 				let {message, type} = reply;
-				setStatus({message, type});
 				if(reply.status === 200 && reply.headers["x-jwt-token"]) {
 					window.localStorage.removeItem("jwt-token");
 					window.localStorage.setItem("jwt-toekn", reply.headers["x-jwt-token"]);
