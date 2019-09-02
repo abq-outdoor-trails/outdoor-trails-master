95fe8d0603f277bc7af12a4e18a35a61136a165e
Will Harris <will.t.harris@protonmail.com>
2019-09-02 14:29:09 -0600

    render a marker component onto map, need to figure out how to programmatically render each point

--- app/src/pages/route-map/RouteMapGL.js
+++ app/src/pages/route-map/RouteMapGL.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import ReactMapGL, {Merker} from 'react-map-gl';
-import * as routeData from "../../image/biketrails_wgs84";
+import ReactMapGL, {Marker} from 'react-map-gl';
+import * as routeData from "../../image/biketrails_wgs84.json";
 
 export const Map = () => {
 	const [viewport, setViewport] = useState({
@@ -21,12 +21,13 @@ export const Map = () => {
 					setViewport(viewport);
 				}}
 			>
-				{routeData.geometry.paths.map(route => (
-					<Marker key={route.attributes.OBJECTID}>
+				{routeData.default.features[0].geometry.paths[0].map(route =>(
+					<Marker latitude={35.082103870000026} longitude={-106.53200461099999}>
 						<div>ROUTE</div>
 					</Marker>
 				))}
+				{console.log(routeData.default.features[0].geometry.paths[0])}
 			</ReactMapGL>
 		</div>
 	)
-}
+};
