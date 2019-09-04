fd6b4fee5f861be42c6d1f7c2009b2382a86eb9a
Chrystal Copeland <canderson73@cnm.edu>
2019-09-04 08:52:25 -0600

    added react action for get-favoriteRoute

---
+++ app/src/shared/actions/get-favoriteRoute.js
@@ -0,0 +1,16 @@
+import {httpConfig} from "../utils/http-config";
+
+export const getFavoriteRouteByFavoriteRouteRouteIdAndFavoriteRouteUserId = () => dispatch => {
+	const {data} =await httpConfig('/apis/favoriteRoute/');
+	dispatch({type: "GET_FAVORITE_ROUTE_BY_FAVORITE_ROUTE_ROUTEID_AND_FAVORITE_ROUTE_USER_ID"})
+};
+
+export const getFavoriteRoutesByRouteId = () => dispatch => {
+	const {data} =await httpConfig('/apis/favoriteRoute/');
+	dispatch({type: "GET_FAVORITE_ROUTE_BY_ROUTE_ID"})
+};
+
+export const getFavoriteRoutesByUserId = () => dispatch => {
+	const {data} =await httpConfig('/apis/favoriteRoute/');
+	dispatch({type: "GET_FAVORITE_ROUTE_BY_USER_ID"})
+};
