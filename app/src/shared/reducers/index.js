0b9b910a53a2fece3ec535968e13667c2c983762
Chrystal Copeland <canderson73@cnm.edu>
2019-09-05 10:57:16 -0600

    added import statement to index.js

--- app/src/shared/reducers/index.js
+++ app/src/shared/reducers/index.js
@@ -2,6 +2,7 @@ import {combineReducers} from "redux"
 import commentReducer from "./comment-reducer";
 import userReducer from "./user-reducer";
 import favoriteRouteReducer from "./favoriteRoute-reducer"
+import routeReducer from "./route-reducer";
 
 export default combineReducers({
 	comments: commentReducer,
